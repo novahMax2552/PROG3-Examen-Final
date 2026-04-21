97d80932a965c104f489bf3316faa7a563b79b90
novahMax2552 <hei.vahatra@gmail.com>
2026-04-21 13:35:58 +0300

    first commit

---
+++ src/main/java/org/prog3/prog3projetfinal/Prog3ProjetFinalApplication.java
@@ -0,0 +1,13 @@
+package org.prog3.prog3projetfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3ProjetFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3ProjetFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/prog3/prog3projetfinal/Prog3ProjetFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prog3.prog3projetfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3ProjetFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
