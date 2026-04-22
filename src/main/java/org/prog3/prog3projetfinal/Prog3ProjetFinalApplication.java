54a9da319b0d1b222cd7bd6c733b831249f652bd
novahMax2552 <hei.vahatra@gmail.com>
2026-04-22 09:46:54 +0300

    Update dependencies in pom.xml and modify Spring Boot application configuration; enhance agricultural federation API specification with new endpoints and versioning details.

--- src/main/java/org/prog3/prog3projetfinal/Prog3ProjetFinalApplication.java
+++ src/main/java/org/prog3/prog3projetfinal/Prog3ProjetFinalApplication.java
@@ -3,7 +3,10 @@ package org.prog3.prog3projetfinal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {
+        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 public class Prog3ProjetFinalApplication {
 
     public static void main(String[] args) {
